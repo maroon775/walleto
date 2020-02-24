d1acd7113787bf981a629852aaa94b4cc4bf0765
Amal Altibaev <maroon775@gmail.com>
2020-02-24 12:06:01 +0300

    create express app

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.use(express.static('static'));
