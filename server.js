00e60da7116d382525e86eb095bba9d34a684f25
Amal Altibaev <maroon775@gmail.com>
2020-02-24 12:31:54 +0300

    fix

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(80, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-app.use(express.static('static'));
+express()
+  .use(express.static(path.join(__dirname, 'static')))
+  .get('/', (req, res) => res.send('Hello World!'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
